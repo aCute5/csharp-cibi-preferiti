69f74769c6ff3387c240a32e13952ecde8324151
dario.costantini <costantini95dario@gmail.com>
2023-03-15 17:59:34 +0100

    adjusting bonus

--- Program.cs
+++ Program.cs
@@ -12,5 +12,14 @@ for ( int i = 0; i < favouriteFood.Length; i++)
 Console.WriteLine("Al primo posto c'è:" + favouriteFood[0]);
 
 Console.WriteLine("L'ultimo dei miei cibi preferiti è:" + favouriteFood[4]);
-Console.WriteLine("A metà classifica c'è:" + favouriteFood[favouriteFood.Length / 2]);
+if (favouriteFood.Length % 2 == 0)
+{
+    Console.WriteLine("A metà classifica c'è:" + favouriteFood[favouriteFood.Length / 2]);
+
+}
+else
+{
+    Console.WriteLine("A metà classifica c'è:" + favouriteFood[favouriteFood.Length / 2] + favouriteFood[favouriteFood.Length / 2 + 1 ]);
+
+}
 
