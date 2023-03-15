aefad175fab389802f53a06789c4e7280ebef679
lenovo <lenovo@DESKTOP-0I6DMFH>
2023-03-15 14:25:46 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] favouriteFood = { "Pizza", "Pasta", "Patate", "Riso", "Frutta" };
+Console.WriteLine(favouriteFood.Length);
+
+for ( int i = 0; i < favouriteFood.Length; i++)
+{
+    Console.WriteLine( (i) + favouriteFood[i] );
+}
+
+Console.WriteLine("Al primo posto c'è:" + favouriteFood[0]);
+
+Console.WriteLine("L'ultimo dei miei cibi preferiti è:" + favouriteFood[4]);
+Console.WriteLine("A metà classifica c'è:" + favouriteFood[favouriteFood.Length / 2]);
+
